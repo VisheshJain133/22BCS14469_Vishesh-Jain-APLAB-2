d9e82abec56e10592a18d6a0b12164cf70ff08d5
VisheshJain133 <116550889+VisheshJain133@users.noreply.github.com>
2025-01-25 13:33:53 +0530

    leetcode83.cpp

--- Linkled List/Remove Duplicate from Sorted List/leetcode83.cpp
+++ Linkled List/Remove Duplicate from Sorted List/leetcode83.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+   ListNode* deleteDuplicates(ListNode* head) {
+     
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* temp = head;
+        
+        
+        while(temp->next != NULL) {
+            if(temp->val == temp->next->val) {
+                ListNode* temp1 = temp->next;
+                temp->next = temp->next->next;
+                delete temp1;
+            } else {
+                temp = temp->next;
+            }
+        }
+        
+        return head;
+     }
