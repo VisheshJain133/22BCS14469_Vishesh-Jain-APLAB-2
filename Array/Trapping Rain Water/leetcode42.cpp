c1ff03ef82af765c748b6398624a3e4f3bbbf447
VisheshJain133 <116550889+VisheshJain133@users.noreply.github.com>
2025-01-27 14:13:08 +0530

    Create leetcode42.cpp

---
+++ Array/Trapping Rain Water/leetcode42.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> l(n,0);
+        vector<int> r(n,0);
+        vector<int> ans(n,0);
+        int res=0;
+        
+        l[0]=height[0];
+        r[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            l[i]=max(l[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            r[i]=max(r[i+1],height[i]);
+        }
+        for(int i=0;i<n;i++){
+            ans[i]=min(l[i],r[i]);
+        }
+        for(int i=0;i<n;i++){
+            ans[i]= ans[i]-height[i];
+            res=res+ans[i];
+        }
+        return res;
+        
+    
+    }
+};
